812965946538c9722e3791fba9d1cdeda8d0cf0d
Leandro Facchinetti <leafac@gmail.com>
2012-06-02 21:42:35 -0300

    End of Kathy's book

---
+++ kathy-sierra-study-notes/chapter-10/test-exercise-11/test/GetJar.java
@@ -0,0 +1,5 @@
+public class GetJar {
+    public static void main(String[] args) {
+        System.out.println(myApp.Foo.d);
+    }
+}
---
+++ kathy-sierra-study-notes/chapter-10/test-exercise-11/test/myApp/Foo.java
@@ -0,0 +1,2 @@
+package myApp;
+public class Foo { public static int d = 8; }
